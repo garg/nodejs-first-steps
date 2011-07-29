7416a5446c9673041354e826f4a0a7e0acf80902
Khurrum M <khurrum1@gmail.com>
2011-07-28 20:08:56 -0400

    server progress

--- helloworld.sh
+++
@@ -1,9 +0,0 @@
-var http = require('http');
-
-http.createServer(function (request, response) {
-  console.log("Request received");
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.write("Hello World");
-  response.end();
-}).listen(8124, "127.0.0.1");
-console.log('Server running at http://127.0.0.1:8124/');
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ function start(route) {
     var pathname = url.parse(request.url).pathname;
     console.log("Request for " + pathname + " received."); 
 
-    route(pathname);
+    route(handle, pathname);
 
     response.writeHead(200, {'Content-Type': 'text/plain'});
     response.write("Hello World");
